a402361fcb516477b9a257be8157d5795d33a913
ArturoDuran90 <78046770+ArturoDuran90@users.noreply.github.com>
2022-02-15 19:05:58 -0700

    changed node modules
    sheeshh nodes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const routes = require('routes');
+const path = require('path');
+// const { ppid } = require('process');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+
+// app.get('/', routes.index);
+// app.get('/create', routes.create);
+// app.post('/create', urlencodedParser, routes.createPerson);
+// app.get('/edit/:id', routes.edit);
+// app.post('/edit/:id', urlencodedParser, routes.editPerson);
+// app.get('/details/:id', routes.details);
+// app.post('/details/:id', urlencodedParser, routes.details);
+// app.get('/delete/:id', routes.delete);
+
+app.listen(3000);
