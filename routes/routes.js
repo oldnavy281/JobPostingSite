c766b0fee8e47e28932eb65812c38917014da2f0
jchanNeumont <jchan@student.neumont.edu>
2022-02-22 18:34:37 -0700

    More Old Code Commented Out

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/data', {
+mongoose.connect('mongodb://localhost/data');
+/*{
     useUnifiedTopology: true,
     useNewUrlParser: true 
-});
-
+} */
 //mongoose.set('useCreateIndex', true);
 //mongoose.set('useFindAndModify', false);
 
